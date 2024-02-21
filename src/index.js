2a9ffdff9c29f10bc9c6b11e426e783ca3bca2b7
lnmduc2 <lnmduc1@gmail.com>
2024-02-21 12:10:35 +0700

    Refractor code: create routes, app/controllers folder

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const { engine } = require("express-handlebars");
 
 const app = express();
 const port = 3000;
+const route = require("./routes");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 // HTTP Logger
 app.use(morgan("combined"));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+); //middleware to console log req.body in POST method
 
 // Template engine
 app.engine(
@@ -22,9 +28,7 @@ app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources\\views"));
 console.log(path.join(__dirname, "resources\\views"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
